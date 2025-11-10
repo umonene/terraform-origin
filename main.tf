1688d3c854ef6d74759aa3b613359f1463529389
umonene <support@taemintech.com>
2025-11-10 13:13:41 -0500

    Added security group to allow Ec2 instance to recieve traffic on port 8080

--- main.tf
+++ main.tf
@@ -18,3 +18,13 @@ resource "aws_instance" "example" {
     Name = "terraform-example"
   }
 }
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
