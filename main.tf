fa56c3e2f7727cd436e729217b8a67fa2ecd207b
umonene <support@taemintech.com>
2025-11-10 12:17:03 -0500

    second commit; i added websever on the file

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t3.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
   tags = {
     Name = "terraform-example"
   }
